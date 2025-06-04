c621754e84e616d59743fd7c1e65619d355b11dd
Goidell <Goidell.Sam@bcg.com>
2025-06-04 18:58:18 -0300

    v1.4.4 404 fix 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true,  // <-- THIS FIXES DEV 404s
-  }
+    historyApiFallback: true, // Enables React Router support during dev
+  },
 })
